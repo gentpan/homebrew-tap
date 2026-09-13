4e9041fb321d74b97ccf906b0ad4a0fde4052f21
gentpan <dev@gentpan.com>
2026-09-13 07:19:23 +0500

    openstats 0.2.0

--- Casks/openstats.rb
+++ Casks/openstats.rb
@@ -7,7 +7,7 @@ cask "openstats" do
   desc "Menu bar system monitor with fan control, keep-awake and cleanup"
   homepage "https://getopenstats.com"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "OpenStats.app"
 
