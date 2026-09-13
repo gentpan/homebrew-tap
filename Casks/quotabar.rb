c181cdc537aa50e7eb91e8f9364bcca7789d3476
gentpan <dev@gentpan.com>
2026-09-13 06:00:26 +0500

    quotabar: repository renamed to gentpan/QuotaBar

--- Casks/quotabar.rb
+++ Casks/quotabar.rb
@@ -2,10 +2,10 @@ cask "quotabar" do
   version "0.5.1"
   sha256 "37f72fa4c15c4884fb3f2220705f33d16d312f8798873ff8231001ad8ce0127c"
 
-  url "https://github.com/gentpan/quotabar/releases/download/v#{version}/QuotaBar-#{version}.zip"
+  url "https://github.com/gentpan/QuotaBar/releases/download/v#{version}/QuotaBar-#{version}.zip"
   name "QuotaBar"
   desc "Menu-bar meter for AI coding provider quotas"
-  homepage "https://github.com/gentpan/quotabar"
+  homepage "https://github.com/gentpan/QuotaBar"
 
   depends_on macos: :sonoma
 
