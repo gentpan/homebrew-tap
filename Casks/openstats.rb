4a3dde295e4e595a913ac8934b6dfa7c15d59c29
gentpan <dev@gentpan.com>
2026-09-15 14:42:12 +0500

    openstats 0.3.0

--- Casks/openstats.rb
+++ Casks/openstats.rb
@@ -1,12 +1,14 @@
 cask "openstats" do
-  version "0.2.0"
-  sha256 "8459f45e038c6ae614690a8b8e7279bb440f780bfafa2c764a47227cd1e4ddfd"
+  version "0.3.0"
+  sha256 "9ef1ab19e6ae6184e416cef34425f4296ab95342ce9aa1285e880e06b09097bf"
 
   url "https://getopenstats.com/download/OpenStats-#{version}.dmg"
   name "OpenStats"
   desc "Menu bar system monitor with fan control, keep-awake and cleanup"
   homepage "https://getopenstats.com"
 
+  # 应用内置在线升级，brew upgrade 默认不再重复升级
+  auto_updates true
   depends_on macos: :sonoma
 
   app "OpenStats.app"
