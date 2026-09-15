414c745dfcd4c1580d7ff7fa590163d36968f3ec
gentpan <dev@gentpan.com>
2026-09-15 16:15:26 +0500

    quotabar 0.5.4

--- Casks/quotabar.rb
+++ Casks/quotabar.rb
@@ -1,6 +1,6 @@
 cask "quotabar" do
-  version "0.5.3"
-  sha256 "ef0ddf359af85ca94a8dda3ca2a2af1c2a99162c4c07ff77137a449177a4c506"
+  version "0.5.4"
+  sha256 "72001abacea07d1511665a2eaed18cd5ea418bf0ae69679a7432db3bcc9dcd5f"
 
   url "https://github.com/gentpan/QuotaBar/releases/download/v#{version}/QuotaBar-#{version}.zip"
   name "QuotaBar"
